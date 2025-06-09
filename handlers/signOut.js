7095f395e50964498dc03ad0e395f5866291db5d
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-06-09 21:46:07 +0530

    signout functionality added

---
+++ handlers/signOut.js
@@ -0,0 +1,29 @@
+const { CognitoIdentityProviderClient, GlobalSignOutCommand } = require('@aws-sdk/client-cognito-identity-provider');
+
+const client = new CognitoIdentityProviderClient({
+	region: 'ap-south-1',
+});
+
+exports.signOut = async (event) => {
+	const { accessToken } = JSON.parse(event.body);
+
+	const params = {
+		// GlobalSignOutRequest
+		AccessToken: accessToken, // required
+	};
+
+	try {
+		const command = new GlobalSignOutCommand(params);
+		await client.send(command);
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify({ msg: 'Sign out successfully' }),
+		};
+	} catch (error) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({ msg: 'Sign out failed!', error: error.message }),
+		};
+	}
+};
